77c891cf136bb63dedabfd7c066410235d8b3db0
Colten Ginnis <78379522+Colten-Developer@users.noreply.github.com>
2021-06-07 10:51:13 -0500

    added cors to app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require('cors')
 const app = express();
 
 const moviesRouter = require("./movies/movies.router");
@@ -8,9 +9,8 @@ const theatersRouter = require("./theaters/theaters.router");
 const notFound = require('./errors/notFound')
 const errorHandler = require('./errors/errorHandler')
 
+app.use(cors())
 app.use(express.json());
-
-//app.use cors
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
